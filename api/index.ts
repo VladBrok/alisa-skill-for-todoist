ee5a1acb062422c425711711c1dd0906ba55812c
VladBrok <vlad.brok99@gmail.com>
2023-09-07 14:57:42 +0300

    make api work

--- api/index.ts
+++ api/index.ts
@@ -15,16 +15,9 @@ dotenv.config();
 //   .then((tasks) => console.log(tasks))
 //   .catch((error) => console.log(error));
 
-// Для асинхронной работы используется пакет micro.
-// const { json } = require("micro");
-
 export default function handler(req: any, res: any) {
-  console.log("THE REQUEST:", req);
-  // Из запроса извлекаются свойства request, session и version.
   const { request, session, version } = req.body;
 
-  // В тело ответа вставляются свойства version и session из запроса.
-  // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
   res.end(
     JSON.stringify({
       version,
@@ -42,28 +35,3 @@ export default function handler(req: any, res: any) {
     })
   );
 }
-
-// // Запуск асинхронного сервиса.
-// module.exports = async (req:any, res:any) => {
-//   // Из запроса извлекаются свойства request, session и version.
-//   const { request, session, version } = await json(req);
-
-//   // В тело ответа вставляются свойства version и session из запроса.
-//   // Подробнее о формате запроса и ответа — в разделе Протокол работы навыка.
-//   res.end(
-//     JSON.stringify({
-//       version,
-//       session,
-//       response: {
-//         // В свойстве response.text возвращается исходная реплика пользователя.
-//         // Если навык был активирован без дополнительной команды,
-//         // пользователю нужно сказать "Hello!".
-//         text: request.original_utterance || "Hello!",
-
-//         // Свойство response.end_session возвращается со значением false,
-//         // чтобы диалог не завершался.
-//         end_session: false,
-//       },
-//     })
-//   );
-// };
