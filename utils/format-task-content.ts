d264c38bc4e96d6f5bd271c027603a8e97bf8f96
VladBrok <vlad.brok99@gmail.com>
2023-09-09 17:25:38 +0300

    strip markdown from an answer

--- utils/format-task-content.ts
+++ utils/format-task-content.ts
@@ -1,7 +1,10 @@
+import removeMd from "remove-markdown";
+
 const MAX_LENGTH = 80;
 
 export default function formatTaskContent(content: string): string {
-  return content.length > MAX_LENGTH
-    ? `${content.slice(0, MAX_LENGTH)}...`
-    : content;
+  const contentWithoutMd = removeMd(content);
+  return contentWithoutMd.length > MAX_LENGTH
+    ? `${contentWithoutMd.slice(0, MAX_LENGTH)}...`
+    : contentWithoutMd;
 }
