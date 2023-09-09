e23ea1abe5ba133f28a384d0175306c382e65b27
VladBrok <vlad.brok99@gmail.com>
2023-09-09 18:56:02 +0300

    test next_page intent

--- api/helpers/handle-utterance.ts
+++ api/helpers/handle-utterance.ts
@@ -4,7 +4,7 @@ import end from "./end-response";
 import getApi from "./get-api";
 import formatTaskContent from "../../utils/format-task-content";
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 7; // TODO: set to 10
 
 export default async function handleUtterance(
   req: VercelRequest,
@@ -16,6 +16,7 @@ export default async function handleUtterance(
   const isNextPage = intents?.["next_page"];
 
   let page = Number(req.cookies["page"]);
+  console.log("0---", page);
   if (Number.isNaN(page) || page < 1) {
     page = 1;
     res.setHeader(
