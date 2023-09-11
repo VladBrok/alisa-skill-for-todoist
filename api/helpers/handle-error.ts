81bac5d945b616d1977a576de853d83ae1eb9b06
VladBrok <vlad.brok99@gmail.com>
2023-09-11 16:23:22 +0300

    handle errors

---
+++ api/helpers/handle-error.ts
@@ -0,0 +1,19 @@
+import { ReqBody, ResBody } from "alice-types";
+import end from "./end-response";
+import { VercelResponse } from "@vercel/node";
+
+export default function handleError(
+  e: unknown,
+  res: VercelResponse,
+  body: ReqBody
+) {
+  console.error(e); // TODO: report error
+  const answer: ResBody = {
+    version: body.version,
+    response: {
+      text: 'Ой, я не смогла обработать запрос.\nПожалуйста, повторите его, или скажите "что ты умеешь" для просмотра доступных действий',
+      end_session: true,
+    },
+  };
+  end(res, answer);
+}
--- api/index.ts
+++ api/index.ts
@@ -5,35 +5,41 @@ import requestAuth from "./helpers/request-auth";
 import handleUtterance from "./helpers/handle-utterance";
 import greetKnownUser from "./helpers/greet-known-user";
 import greetNewUser from "./helpers/greet-new-user";
+import handleError from "./helpers/handle-error";
 
-// TODO: handle errors
 // TODO: if todoist api returns error caused by auth -> request auth from the user
 // TODO: add yandex user name (fetch it by user_id) to some answers (where it would be appropriate)
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const body = req.body as ReqBody;
 
-  const supportsAuth = Boolean(body.meta.interfaces.account_linking);
-  if (!supportsAuth) {
-    authNotSupported(res, body);
-    return;
-  }
+  try {
+    throw new Erro("oops");
 
-  const authenticated = Boolean(body.session.user?.access_token);
-  if (!authenticated) {
-    requestAuth(res, body);
-    return;
-  }
+    const supportsAuth = Boolean(body.meta.interfaces.account_linking);
+    if (!supportsAuth) {
+      authNotSupported(res, body);
+      return;
+    }
 
-  // @ts-ignore
-  if (body.account_linking_complete_event) {
-    await greetNewUser(res, body);
-    return;
-  }
+    const authenticated = Boolean(body.session.user?.access_token);
+    if (!authenticated) {
+      requestAuth(res, body);
+      return;
+    }
 
-  if (!body.request.original_utterance) {
-    await greetKnownUser(res, body);
-    return;
-  }
+    // @ts-ignore
+    if (body.account_linking_complete_event) {
+      await greetNewUser(res, body);
+      return;
+    }
 
-  await handleUtterance(res, body);
+    if (!body.request.original_utterance) {
+      await greetKnownUser(res, body);
+      return;
+    }
+
+    await handleUtterance(res, body);
+  } catch (e) {
+    handleError(e, res, body);
+  }
 }
