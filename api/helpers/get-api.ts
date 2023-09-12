be1e13091d68301e47dc9eef5f7db7c882a2cb0e
VladBrok <vlad.brok99@gmail.com>
2023-09-12 13:51:13 +0300

    test todoist api error

--- api/helpers/get-api.ts
+++ api/helpers/get-api.ts
@@ -2,7 +2,7 @@ import { TodoistApi } from "@doist/todoist-api-typescript";
 import { ReqBody } from "alice-types";
 
 export default function getApi(body: ReqBody) {
-  const apiToken = body.session.user?.access_token + "blabla" || "";
+  const apiToken = body.session.user?.access_token || "";
   const api = new TodoistApi(apiToken);
   return api;
 }
