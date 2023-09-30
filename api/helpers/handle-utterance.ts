bf6ae1f480f3f8acb6642617c84f2f0dc7d82428
VladBrok <vlad.brok99@gmail.com>
2023-09-30 14:17:06 +0300

    handle help intent

--- api/helpers/handle-utterance.ts
+++ api/helpers/handle-utterance.ts
@@ -22,6 +22,7 @@ export default async function handleUtterance(
   const isCloseTask = intents?.["close_task"];
   const isUpdateTask = intents?.["update_task"];
   const isGetExpired = intents?.["get_expired"];
+  const isHelp = intents?.["help"];
   let responseText = t("unhandle_utterance");
   let responseTts = "";
 
@@ -200,6 +201,9 @@ export default async function handleUtterance(
         };
       }
     ));
+  } else if (isHelp) {
+    responseText = t("help");
+    responseTts = applyTts(responseText);
   }
 
   const answer: ResBody = {
